43db17283fabb3ab5c435299faa9d9376904aa04
annie_wu0509@163.com <annie_wu0509@163.com>
2026-07-17 16:16:51 +0800

    update "allure4"

--- run_test_01.py
+++ run_test_01.py
@@ -2,4 +2,4 @@ import pytest
 
 if __name__ == "__main__":
     # pytest.main(["testDemo_01.py","-v","-s","--html=./test_report.html"])
-    pytest.main(["testDemo.py", "-v", "-s", "--alluredir=/workspace/report/allure_raw"])
+    pytest.main(["testDemo_01.py", "-v", "-s", "--alluredir=/workspace/report/allure_raw"])
